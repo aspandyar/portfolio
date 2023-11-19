b20a459010d035c6c44f47fc4b1fa2e9a7fb9b68
aspandyar <aspandyart@gmail.com>
2023-11-19 18:56:39 +0600

    type js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // Change color while scrool
     $(window).on('scroll', function(){
         var scroll = $(window).scrollTop();
         if(scroll>=50){
@@ -7,4 +8,18 @@ $(document).ready(function(){
             $(".sticky").removeClass("stickyadd");
         }
     });
+
+
+    // Type JS
+    var typed = new Typed(".element", {
+        strings: ["Aspandyar Sharip", "a Backend Developer", "a Frontend(mb) Developer"],
+        smartBackspace:true,
+        typeSpeed:100,
+        backSpeed:100,
+        loop:true,
+        loopCount:Infinity,
+        startDelay:1000
+    });
+
+
 });
