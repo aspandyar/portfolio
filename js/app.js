962bcc81874dbc67663280e50e4cc5477971eea7
aspandyar <aspandyart@gmail.com>
2023-11-19 20:42:26 +0600

    break point

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    // Change color while scrool
+    // Change nav color while scrool
     $(window).on('scroll', function(){
         var scroll = $(window).scrollTop();
         if(scroll>=50){
@@ -9,8 +9,7 @@ $(document).ready(function(){
         }
     });
 
-
-    // Type JS
+    // Type JS in home page
     var typed = new Typed(".element", {
         strings: ["Aspandyar Sharip", "a Backend Developer", "a Frontend(mb) Developer"],
         smartBackspace:true,
@@ -21,5 +20,18 @@ $(document).ready(function(){
         startDelay:1000
     });
 
+    // progress bar in my skills
+    var waypoint = new Waypoint({ //waypoint logic to static update progress bar
+        element: document.getElementById('exp-id'),
+        handler: function() {
+            var p = document.querySelectorAll('.progress-bar');
 
+            p[0].setAttribute("style", "width:70%;transition:1s all")
+            p[1].setAttribute("style", "width:80%;transition:1.5s all")
+            p[2].setAttribute("style", "width:50%;transition:2s all")
+            p[3].setAttribute("style", "width:65%;transition:2.5s all")
+            p[4].setAttribute("style", "width:50%;transition:3s all")
+        },
+        offset:'90%' // need to update earlier then needing place was got
+      })
 });
